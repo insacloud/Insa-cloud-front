4661cd22affed57c8f9909065a81228ade951232
terceser <t.m.hoang.92@gmail.com>
2015-10-21 12:33:22 +0200

    mosaic
    mosaic

--- app/src/main/java/insa/cloud/activity/LoginActivity.java
+++ app/src/main/java/insa/cloud/activity/LoginActivity.java
@@ -1,11 +1,13 @@
 package insa.cloud.activity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -139,7 +141,7 @@ public class LoginActivity extends AppCompatActivity{
         btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), SendPhotoActivity.class);
+                Intent i = new Intent(getApplicationContext(), EventDetailsActivity.class);
                 startActivity(i);
                 finish();
             }
--- app/src/main/java/insa/cloud/fragment/EventMosaicFragment.java
+++ app/src/main/java/insa/cloud/fragment/EventMosaicFragment.java
@@ -3,12 +3,15 @@ package insa.cloud.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.Random;
 
 import insa.cloud.R;
 import insa.cloud.global.VolleyController;
@@ -18,9 +21,17 @@ import insa.cloud.global.VolleyController;
  */
 public class EventMosaicFragment extends Fragment {
     ImageLoader imageLoader = VolleyController.getInstance().getImageLoader();
-    private String testUrl = "http://lorempixel.com/90/120/abstract/";
+    private String testUrl = "http://lorempixel.com/100/90/?random=";
     public EventMosaicFragment() {
         // Required empty public constructor
+    }
+
+    public String getTestUrl(){
+        Random generator = new Random();
+        int number = generator.nextInt(10000);
+        String myUrl = testUrl + Integer.toString(number);
+        Log.d("URL",myUrl);
+        return myUrl;
     }
 
 
@@ -29,6 +40,7 @@ public class EventMosaicFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_event_mosaic, container, false);
+
         if (imageLoader == null)
             imageLoader = VolleyController.getInstance().getImageLoader();
         testData(rootView);
@@ -38,69 +50,69 @@ public class EventMosaicFragment extends Fragment {
 
     public void testData(View rootView) {
         NetworkImageView image = (NetworkImageView) rootView.findViewById(R.id.image1_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image1_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image1_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image1_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image1_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
 
         image = (NetworkImageView) rootView.findViewById(R.id.image2_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image2_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image2_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image2_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image2_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
 
         image = (NetworkImageView) rootView.findViewById(R.id.image3_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image3_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image3_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image3_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image3_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
 
         image = (NetworkImageView) rootView.findViewById(R.id.image4_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image4_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image4_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image4_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image4_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
 
         image = (NetworkImageView) rootView.findViewById(R.id.image5_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image5_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image5_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image5_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image5_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
 
         image = (NetworkImageView) rootView.findViewById(R.id.image6_1);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image6_2);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image6_3);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image6_4);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
         image = (NetworkImageView) rootView.findViewById(R.id.image6_5);
-        image.setImageUrl(testUrl, imageLoader);
+        image.setImageUrl(getTestUrl(), imageLoader);
     }
 }
